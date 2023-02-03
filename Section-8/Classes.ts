52ffc414b8c2dc7fdce2444449a3b2c19e8ccc7c
Vidhish-Trivedi <Vidhish.Trivedi@iiitb.ac.in>
2023-02-04 01:14:08 +0530

    Methods in classes

--- Section-8/Classes.ts
+++ Section-8/Classes.ts
@@ -1,26 +1,39 @@
 // Class Definition.
 class Vehicle {
-    drive(): void {
+    public drive(): void {
         console.log("driving...");
     }
-    honk(): void {
+    
+    // Accessible in this class and its children.
+    protected honk(): void {
         console.log("honking...");
+    }
+
+    // Accessible only within this class.
+    private breakVehicle(): void {
+        console.log("Break...");
     }
 };
 
 // Inheritance.
 class Car extends Vehicle {
     // Overriding an inherited method.
-    drive(): void {
+    public drive(): void {
         console.log("vroom...");
     }
+
+    public startCar(): void {
+        // Access protected method of parent class.
+        this.honk();
+    }
+
 };
 
 // Instance of a class.
 const vehicle_1 = new Vehicle();
 vehicle_1.drive();
-vehicle_1.honk();
+// vehicle_1.honk();    // TRY UNCOMMENTING THIS LINE.
 
 const car_1 = new Car();
 car_1.drive();
-car_1.honk();
+car_1.startCar();
--- Section-8/ReadMe.txt
+++ Section-8/ReadMe.txt
@@ -1,4 +1,9 @@
 CLASSES:
     A class is a blueprint to create an object with some fields/attributes and methods.
-    
+
+    MODIFIERS:
+        Key words (public, private, protected {default is public}).
+        Used to restrict access to attributes and methods.
+        When using Inheritance, we should not change the modifier while overriding an existing method/attribute.
+        
 
