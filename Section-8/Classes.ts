f0ab4e06342f7ea946fb68c4c23b010356623fda
Vidhish-Trivedi <Vidhish.Trivedi@iiitb.ac.in>
2023-02-04 01:32:08 +0530

    Completed introduction to Classes

--- Section-8/Classes.ts
+++ Section-8/Classes.ts
@@ -1,5 +1,18 @@
 // Class Definition.
 class Vehicle {
+    // Traditionally, we might want to initialise an attribute on declaration.
+    // color: string = "red";    // Set a default value.
+    protected color: string;
+
+    // Constructor for this class.
+    constructor(color: string) {
+        this.color = color;
+    }
+
+    // ALTERNATE WAY: Here, we won't need to define the attribute seperately and then assign its value. Body of constructor can be empty.
+    // >> constructor(private color: string){}
+    // Modifiers work the same way with attributes also.
+
     public drive(): void {
         console.log("driving...");
     }
@@ -7,6 +20,10 @@ class Vehicle {
     // Accessible in this class and its children.
     protected honk(): void {
         console.log("honking...");
+    }
+
+    public getColor(): string {
+        return(this.color);
     }
 
     // Accessible only within this class.
@@ -17,6 +34,8 @@ class Vehicle {
 
 // Inheritance.
 class Car extends Vehicle {
+    // When no constructor is defined, the DEFAULT constructor is called or the PARENT class constructor is called.
+
     // Overriding an inherited method.
     public drive(): void {
         console.log("vroom...");
@@ -26,14 +45,29 @@ class Car extends Vehicle {
         // Access protected method of parent class.
         this.honk();
     }
-
 };
 
+class Bus extends Vehicle {
+    public wheels: number;
+
+    // Every constructor of a child class will call the corresponding constructor of the parent class using super().
+    constructor(wheels: number, color: string) {
+        super(color);
+        this.wheels = wheels;
+    }
+}
+
 // Instance of a class.
-const vehicle_1 = new Vehicle();
+const vehicle_1 = new Vehicle("yellow");
 vehicle_1.drive();
 // vehicle_1.honk();    // TRY UNCOMMENTING THIS LINE.
+console.log(vehicle_1.getColor());
 
-const car_1 = new Car();
+const car_1 = new Car("red");
 car_1.drive();
 car_1.startCar();
+console.log(car_1.getColor());
+
+const bus_1 = new Bus(3, "blue");
+bus_1.drive();
+console.log(bus_1.getColor());
--- Section-8/ReadMe.txt
+++ Section-8/ReadMe.txt
@@ -5,5 +5,6 @@ CLASSES:
         Key words (public, private, protected {default is public}).
         Used to restrict access to attributes and methods.
         When using Inheritance, we should not change the modifier while overriding an existing method/attribute.
-        
 
+    WHEN TO USE CLASSES:
+        Object-Oriented Principles!
